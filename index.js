a065a7d94fa87fde6a6ff3376ba5d8b78dbbae62
Charbel <charbelghosn24@hotmail.com>
2024-07-29 17:41:27 -0700

    First steps completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// TODO: this file! :)
+const form =document.querySelector('form')
+const numberBank=[]
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    const number = e.currentTarget.number.value
+    numberBank.push(number)
+    e.currentTarget.reset()
+    showNumberBank()
+})
+
+function showNumberBank(){
+    const outputEL = document.querySelector("#numberBank output")
+    outputEL.value = numberBank.join("")
+}
+
